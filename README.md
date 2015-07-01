d520a0d9ab0e68a902844f029d27cb24bb99079e
steve <vanhuynhduy@gmail.com>
2015-07-01 18:03:31 +0700

    complete version 1

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # aws-eb-cleanup
-A tool to delete old AWS Elastic Beanstalk Application Versions and S3 files
+A tool to delete old AWS Elastic Beanstalk Application Versions (and their source bundles in S3)
+
+# Features
+
+1. Clean up older versions of [Elastic Beanstalk](http://aws.amazon.com/documentation/elastic-beanstalk/) applications. These versions take up your S3 storage.
+2. Allow you to specify the number of day that a version is considered *old*. The script will **never** delete a running version.
+
+# Requirements
+
+1. You need to set your AWS ACCESS KEY ID and ACCESS KEY SECRET. Follow [this guide](https://github.com/aws/aws-sdk-go) to set them
+2. Your AWS ACCESS KEY should have respective permissions. The following ElasticBeanstalk permissions must be granted: **DescribeApplicationVersions**, **DescribeEnvironments**, **DeleteApplicationVersions** (if you want to do the actual clean up)  
+3. AWS_REGION environment variable must be set. Default is *us-east-1*. You can also pass this value to global flag `--region` when running `ebclean`
+
+# Usage
+
+1. Clone the repository
+2. Install godep (https://github.com/tools/godep)
+3. Run `godep restore`
+4. Run `go install`
+5. Execute `ebclean help` in your terminal and follow the instruction there
+
+# Known Limitations
+
+1. No bulk clean-up. If you need to clean up old versions for multiple Elastic Beanstalk Applications, you need to do it one by one. 
+
+# Credits
+
+The project uses the following awesome open source libraries:
+
+1. [AWS Official Go SDK](https://github.com/aws/aws-sdk-go)
+2. [Termtables](https://github.com/apcera/termtables)
+3. [Cli](github.com/codegangsta/cli)
+4. [Godep](https://github.com/tools/godep)
+
+
+
--- ebclean.go
+++ ebclean.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/apcera/termtables"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/awsutil"
 	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
 	"github.com/codegangsta/cli"
+	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -19,36 +22,181 @@ func main() {
 		cli.StringFlag{
 			Name:   "region, r",
 			Value:  "us-east-1",
-			Usage:  "AWS region",
+			Usage:  "AWS region. Default is us-east-1",
 			EnvVar: "AWS_REGION",
 		},
 	}
 
 	app.Commands = []cli.Command{
 		{
-			Name:    "list",
-			Aliases: []string{"ls"},
-			Usage:   "list application, environment and how many versions, which is running",
+			Name:  "inspect",
+			Usage: "inspect an application",
 			Action: func(c *cli.Context) {
 				eb := elasticbeanstalk.New(&aws.Config{Region: c.GlobalString("region")})
-				resp, err := eb.DescribeApplications(nil)
+				appName := c.Args().First()
 
-				if err != nil {
-					if awsErr, ok := err.(awserr.Error); ok {
-						fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+				duration := c.Int("duration")
 
-						if reqErr, ok := err.(awserr.RequestFailure); ok {
-							fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
-						}
-					} else {
-						fmt.Println(err.Error())
-					}
+				//if flag is not set
+				if duration == 0 {
+					duration = 30
 				}
 
-				fmt.Println(awsutil.StringValue(resp))
+				inspect(eb, appName, duration)
+			},
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:  "duration, d",
+					Value: 30,
+					Usage: `number of days from current time that a version is considered obsolete. For example, if set to 30, 
+			all versions older than 30 days from current time will be marked to delete. Default is 30`,
+				},
+			},
+		},
+		{
+			Name:  "clean",
+			Usage: "clean up all old versions of an application",
+			Action: func(c *cli.Context) {
+				eb := elasticbeanstalk.New(&aws.Config{Region: c.GlobalString("region")})
+				appName := c.Args().First()
+
+				duration := c.Int("duration")
+
+				//if flag is not set
+				if duration == 0 {
+					duration = 30
+				}
+
+				disposableVersionList := inspect(eb, appName, duration)
+
+				if len(disposableVersionList) > 0 {
+					deleteSourceBundle := c.BoolT("delete-source-bundle")
+
+					//if the flag is not set, default is true
+					if !deleteSourceBundle {
+						deleteSourceBundle = true
+					}
+
+					clean(eb, appName, disposableVersionList, deleteSourceBundle)
+				} else {
+					fmt.Println("No version can be deleted")
+				}
+			},
+			Flags: []cli.Flag{
+				cli.BoolTFlag{
+					Name:  "delete-source-bundle",
+					Usage: "if true, this will delete source bundle for a given application version in AWS S3. Default is true.",
+				},
+				cli.IntFlag{
+					Name:  "duration, d",
+					Value: 30,
+					Usage: `number of days from current time that a version is considered obsolete. For example, if set to 30, 
+			all versions older than 30 days from current time will be marked to delete. Default is 30`,
+				},
 			},
 		},
 	}
 
 	app.Run(os.Args)
+}
+
+// inspect and print out details of disposable versions for a given application
+func inspect(eb *elasticbeanstalk.ElasticBeanstalk, appName string, duration int) []*elasticbeanstalk.ApplicationVersionDescription {
+	applicationVersionResp, err := eb.DescribeApplicationVersions(&elasticbeanstalk.DescribeApplicationVersionsInput{ApplicationName: &appName})
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		os.Exit(1)
+	}
+
+	environmentResp, err := eb.DescribeEnvironments(&elasticbeanstalk.DescribeEnvironmentsInput{ApplicationName: &appName})
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		os.Exit(1)
+	}
+
+	var disposableVersions = make([]*elasticbeanstalk.ApplicationVersionDescription, 0)
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString(fmt.Sprintf("Application Name: %s\n", appName))
+	buffer.WriteString(fmt.Sprintf("Total version: %d\n", len(applicationVersionResp.ApplicationVersions)))
+
+	table := termtables.CreateTable()
+
+	table.AddHeaders("Version Label", "Date Created", "Environment")
+
+	for _, version := range applicationVersionResp.ApplicationVersions {
+		var found bool = false
+		for _, environment := range environmentResp.Environments {
+			if *version.VersionLabel == *environment.VersionLabel {
+				table.AddRow(*version.VersionLabel, version.DateCreated, *environment.EnvironmentName)
+				found = true
+			}
+		}
+
+		if !found {
+			if time.Now().After(version.DateCreated.AddDate(0, 0, duration)) {
+				disposableVersions = append(disposableVersions, version)
+				table.AddRow(*version.VersionLabel, version.DateCreated, "*Disposable*")
+			} else {
+				table.AddRow(*version.VersionLabel, version.DateCreated, "Not disposable")
+			}
+		}
+	}
+
+	buffer.WriteString(table.Render())
+	buffer.WriteString(fmt.Sprintf("Disposable version count: %d\n", len(disposableVersions)))
+
+	fmt.Println(buffer.String())
+
+	return disposableVersions
+}
+
+//clean up all Application Version inside "versions" slice
+func clean(eb *elasticbeanstalk.ElasticBeanstalk, appName string, versions []*elasticbeanstalk.ApplicationVersionDescription, deleteSourceBundle bool) {
+	for _, version := range versions {
+		var buffer bytes.Buffer
+
+		buffer.WriteString(fmt.Sprintf("Deleting version %s .......... ", *version.VersionLabel))
+
+		_, err := eb.DeleteApplicationVersion(&elasticbeanstalk.DeleteApplicationVersionInput{
+			ApplicationName:    &appName,
+			DeleteSourceBundle: &deleteSourceBundle,
+			VersionLabel:       version.VersionLabel,
+		})
+
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok {
+				log.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+				buffer.WriteString("Error")
+				if reqErr, ok := err.(awserr.RequestFailure); ok {
+					log.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+					buffer.WriteString("Error")
+				}
+			} else {
+				log.Println(err.Error())
+				buffer.WriteString("Error")
+			}
+		} else {
+			buffer.WriteString("OK")
+		}
+
+		fmt.Println(buffer.String())
+	}
 }
